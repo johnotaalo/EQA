56242aee22c4163db3974c4423dda59f4801d960
DESKTOP-N3QOKFT\User <marekawilly@gmail.com>
2017-02-11 15:21:09 -0800

    Base_url not working

--- application/modules/Home/views/home_v.php
+++ application/modules/Home/views/home_v.php
@@ -3,34 +3,14 @@
 <head>
 	<title>WELCOME TO EQA</title>
 	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Muli|Open+Sans|Poiret+One" rel="stylesheet">
-	<style type="text/css">
-		body{
-			font-family: 'Poiret One', cursive;
-			font-family: 'Josefin Sans', sans-serif;
-			font-family: 'Open Sans', sans-serif;
-			font-family: 'Muli', sans-serif;
-			margin: 0;
-			padding: 0;
-		}
-		.welcome-div{
-			position: fixed;
-			width: 100%;
-			height: 100%;
-			line-height: 100%;
-			background: #EEEEEE;
-		}
+	<link rel="stylesheet" href="<?php echo base_url().'assets/plugin/bootstrap-3.3.7/css/bootstrap.min.css'?>" />
+	<link rel="stylesheet" href="<?php echo base_url().'assets/plugin/jquery-ui.1.12.1/jquery-ui.min.css'?>" />
+	<link rel="stylesheet" href="<?php echo base_url().'assets/css/main.css'?>" />
 
-		.welcome-div h1{
-			position: absolute;
-			float: left;
-			top: 50%;
-			left: 50%;
-			transform: translate(-50%, -50%);
-			font-size: 80px;
-			color: #616161;
-		}
-	</style>
-</head>
+    <script type="text/javascript" charset="utf-8" src="<?php echo base_url().'assets/js/jquery.js'?>"></script>
+    <script type="text/javascript" charset="utf-8" src="<?php echo base_url().'assets/plugin/jquery-ui.1.12.1/jquery-ui.min.js'?>"></script>
+    <script type="text/javascript" charset="utf-8" src="<?php echo base_url().'assets/plugin/bootstrap-3.3.7/js/bootstrap.min.js'?>"></script>
+    <script type="text/javascript" charset="utf-8" src="<?php echo base_url().'assets/js/main.js'?>"></script>
 <body>
 	<div class = "welcome-div">
 		<center><h1>WELCOME TO EQA</h1></center>
