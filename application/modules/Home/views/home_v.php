68dc597fcce50a369a48490b00b09ef7c7188a03
Chris Otaalo <john.otaalo@strathmore.edu>
2017-02-11 11:10:13 +0300

    First Commit

---
+++ application/modules/Home/views/home_v.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>WELCOME TO EQA</title>
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Muli|Open+Sans|Poiret+One" rel="stylesheet">
+	<style type="text/css">
+		body{
+			font-family: 'Poiret One', cursive;
+			font-family: 'Josefin Sans', sans-serif;
+			font-family: 'Open Sans', sans-serif;
+			font-family: 'Muli', sans-serif;
+			margin: 0;
+			padding: 0;
+		}
+		.welcome-div{
+			position: fixed;
+			width: 100%;
+			height: 100%;
+			line-height: 100%;
+			background: #EEEEEE;
+		}
+
+		.welcome-div h1{
+			position: absolute;
+			float: left;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			font-size: 80px;
+			color: #616161;
+		}
+	</style>
+</head>
+<body>
+	<div class = "welcome-div">
+		<center><h1>WELCOME TO EQA</h1></center>
+	</div>
+</body>
+</html>
