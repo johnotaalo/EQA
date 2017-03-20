8fdfcb1c9432e32ef40b8a22a3f4d1c6a16f7cf2
WillyMareka <marekawilly@gmail.com>
2017-03-20 19:10:08 +0300

    Prepared admin Dashboard and added dummy data

---
+++ application/modules/Dashboard/views/admin_dashboard.php
@@ -0,0 +1,109 @@
+<div class="row">
+    <div class="col-sm-6 col-lg-4">
+        <div class="card">
+            <div class="card-block">
+                <div class="h4 m-0">9</div>
+                <div class="pb-1">Users Pending Approval</div>
+                <progress class="progress progress-xs progress-warning" value="100" max="100"></progress>
+                <!-- <small class="text-muted">Users Pending Approval</small> -->
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-lg-4">
+        <div class="card">
+            <div class="card-block">
+                <div class="h4 m-0">12</div>
+                <div class="pb-1">New Equipmets registered by Users</div>
+                <progress class="progress progress-xs progress-primary" value="100" max="100"></progress>
+                <!-- <small class="text-muted">New Equipmets registered by Users</small> -->
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-lg-4">
+        <div class="card">
+            <div class="card-block">
+                <div class="h4 m-0">220</div>
+                <div class="pb-1">Submitted Readiness Forms by Participants</div>
+                <progress class="progress progress-xs progress-success" value="100" max="100"></progress>
+                <!-- <small class="text-muted">Submitted Readiness Forms by Participants</small> -->
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+<div class="row">
+    <div class="col-sm-6 col-lg-4">
+        <div class="card">
+            <div class="card-block">
+                <div class="h4 m-0">215</div>
+                <div class="pb-1">Participants who Recieved PT Panels at Site</div>
+                <progress class="progress progress-xs progress-success" value="100" max="100"></progress>
+                <!-- <small class="text-muted">Participants who Recieved PT Panels at Site</small> -->
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-lg-4">
+        <div class="card">
+            <div class="card-block">
+                <div class="h4 m-0">5</div>
+                <div class="pb-1">Participants not Recieved their PT Panels at Site</div>
+                <progress class="progress progress-xs progress-danger" value="100" max="100"></progress>
+                <!-- <small class="text-muted">Participants not Recieved their PT Panels at Site</small> -->
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-lg-4">
+        <div class="card">
+            <div class="card-block">
+                <div class="h4 m-0">200</div>
+                <div class="pb-1">Participants submitted Pending QA review</div>
+                <progress class="progress progress-xs progress-success" value="100" max="100"></progress>
+                <!-- <small class="text-muted">Participants submitted Pending QA review</small> -->
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+<div class="row">
+    <div class="col-sm-6 col-lg-4">
+        <div class="card">
+            <div class="card-block">
+                <div class="h4 m-0">15</div>
+                <div class="pb-1">Participants yet to submit QA review</div>
+                <progress class="progress progress-xs progress-warning" value="100" max="100"></progress>
+                <!-- <small class="text-muted">Participants yet to submit QA review</small> -->
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-lg-4">
+        <div class="card">
+            <div class="card-block">
+                <div class="h4 m-0">212</div>
+                <div class="pb-1">Participants completed and reviewed QA</div>
+                <progress class="progress progress-xs progress-success" value="100" max="100"></progress>
+                <!-- <small class="text-muted">Participants that have completed and reviewed</small> -->
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-6 col-lg-4">
+        <div class="card">
+            <div class="card-block">
+                <div class="h4 m-0">3</div>
+                <div class="pb-1">Participants not completed and reviewed QA</div>
+                <progress class="progress progress-xs progress-alert" value="100" max="100"></progress>
+                <!-- <div class="pb-1">Participants that have not completed and reviewed</div> -->
+            </div>
+        </div>
+    </div>
+
+</div>
