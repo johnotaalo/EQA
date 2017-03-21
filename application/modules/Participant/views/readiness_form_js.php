a4c6830265bef95db919f4ffb80b4ccc56ad0cff
WillyMareka <marekawilly@gmail.com>
2017-03-22 02:57:12 +0300

    Able to hack a logic that worked for the YES/NO questionnaire values. Sleeping at 3:00am

--- application/modules/Participant/views/readiness_form_js.php
+++ application/modules/Participant/views/readiness_form_js.php
@@ -10,21 +10,67 @@
 		question_4_2_0.disabled = true;
 		question_4_2_1.disabled = true;
 
+		$(question11).val();
+        var question11= $(this).attr('question_1_1');
+        var question21= $(this).attr('question_2_1');
+        var question31= $(this).attr('question_2_1');
+
 
 	$('input[type="radio"]').click(function() {
-       if(($(this).attr('id') == 'question_1_0') || ($(this).attr('id') == 'question_2_0') || ($(this).attr('id') == 'question_3_0')) {
+       if(($(this).attr('id') == 'question_1_0')) {
 	   	    $divcheck = $('#checkNoAnswers').is(":visible"); 	
 	       	if(!($divcheck)){
-				activatequestion();  
+				activatequestion();
 	       	}
             
-       	}else if(($(this).attr('id') == 'question_1_1') && ($(this).attr('id') == 'question_2_1') && ($(this).attr('id') == 'question_3_1')){
-       		$divcheck = $('#checkNoAnswers').is(":visible"); 	
-	       	if($divcheck){
-				deactivatequestion();  
+       	}
+   });
+
+	$('input[type="radio"]').click(function() {
+       if(($(this).attr('id') == 'question_2_0')) {
+	   	    $divcheck = $('#checkNoAnswers').is(":visible"); 	
+	       	if(!($divcheck)){
+				activatequestion();
 	       	}
-               
-       }
+            
+       	}
+   });
+
+	$('input[type="radio"]').click(function() {
+       if(($(this).attr('id') == 'question_3_0')) {
+	   	    $divcheck = $('#checkNoAnswers').is(":visible"); 	
+	       	if(!($divcheck)){
+				activatequestion();
+	       	}
+            
+       	}
+   });
+
+	$('input[type="radio"]').click(function() {
+	 	if(($(this).attr('id') == 'question_1_1')){
+       		$checkNo = checkIfNoAnswer();
+	       	if(!($checkNo)){
+	       		deactivatequestion(); 
+	       	}
+       	}
+   });
+
+	$('input[type="radio"]').click(function() {
+	 	if(($(this).attr('id') == 'question_2_1')){
+       		$checkNo = checkIfNoAnswer();
+	       	if(!($checkNo)){
+	       		deactivatequestion(); 
+	       	}
+       	}
+   });
+
+	$('input[type="radio"]').click(function() {
+	 	if(($(this).attr('id') == 'question_3_1')){
+       		$checkNo = checkIfNoAnswer();
+	       	if(!($checkNo)){
+	       		deactivatequestion(); 
+	       	}
+       	}
    });
 
 
@@ -41,6 +87,18 @@
 		question_4_1.disabled = true;
 		question_4_2_0.disabled = true;
 		question_4_2_1.disabled = true;
+	}
+
+	function checkIfNoAnswer(){
+		$que1 = $('#question_1_0').is(":checked"); 
+		$que2 = $('#question_2_0').is(":checked"); 
+		$que3 = $('#question_3_0').is(":checked");
+
+		if($que1 || $que2 || $que3){
+			return true;
+		} else{
+			return false;
+		}
 	}
 
 
