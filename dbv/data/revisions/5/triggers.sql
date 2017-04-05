91d24b86f1acd0618fcb8233e80281b95502aa56
Chrispine Otaalo <john.otaalo@strathmore.edu>
2017-04-05 16:58:32 +0300

    DBV changes

--- dbv/data/revisions/5/triggers.sql
+++ dbv/data/revisions/5/triggers.sql
@@ -1,9 +1,9 @@
-CREATE DEFINER=`homestead`@`%` TRIGGER `eqa`.`participant_readiness_BEFORE_INSERT` BEFORE INSERT ON `participant_readiness` FOR EACH ROW
+CREATE TRIGGER `eqa`.`participant_readiness_BEFORE_INSERT` BEFORE INSERT ON `participant_readiness` FOR EACH ROW
 BEGIN
 	SET new.uuid := (SELECT UUID());
 END;
 
-CREATE DEFINER=`homestead`@`%` TRIGGER `eqa`.`participant_readiness_responses_BEFORE_INSERT` BEFORE INSERT ON `participant_readiness_responses` FOR EACH ROW
+CREATE TRIGGER `eqa`.`participant_readiness_responses_BEFORE_INSERT` BEFORE INSERT ON `participant_readiness_responses` FOR EACH ROW
 BEGIN
 	SET new.uuid := (SELECT UUID());
 END;
