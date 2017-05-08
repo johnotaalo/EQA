4d3d02f2eded301814aa699bc9bb7c4dd1613e99
WillyMareka <marekawilly@gmail.com>
2017-05-08 11:07:06 +0300

    Ill just wait for the internet to come back, and research

--- application/modules/Home/controllers/Home.php
+++ application/modules/Home/controllers/Home.php
@@ -73,62 +73,6 @@ class Home extends MY_Controller {
     }
 
 
-    //Function used to revise on how the export the pdf
-
-    // file-> /uploads/docs/NHRL-PT-SOP-19042017.pdf
-
-    // function dispatchList($pt_round_uuid){
-    //     $table_header = [
-    //         '#',
-    //         'Recepient Name',
-    //         'Recepient Phone',
-    //         'Facility Name',
-    //         'G4S Branch'
-    //     ];
-
-    //     $this->db->where('pt_round_uuid', $pt_round_uuid);
-    //     $this->db->where('status_code', 2);
-    //     $dispatch_ready = $this->db->get('pt_ready_participants')->result();
-    //     $table_body = [];
-    //     if ($dispatch_ready) {
-    //         $counter = 1;
-    //         foreach ($dispatch_ready as $ready) {
-    //             $table_body[] = [
-    //                 $counter,
-    //                 "$ready->participant_fname $ready->participant_lname",
-    //                 $ready->participant_phonenumber,
-    //                 $ready->facility_name,
-    //                 $ready->G4S_branch_name
-    //             ];
-
-    //             $counter++;
-    //         }
-    //     }
-
-    //     $this->load->library('table');
-    //     $this->load->config('table');
-
-    //     $template = $this->config->item('default');
-
-    //     $this->table->set_heading($table_header);
-    //     $this->table->set_template($template);
-
-    //     $viewData['dispatch_list'] = $this->table->generate($table_body);
-    //     $pdf_view = $this->load->view('PTRounds/paneltracking/dispatch_list_v', $viewData, TRUE);       
-    //     $data['document_title'] = "Dispatch List - Panel Tracking - " . date('d-m-Y');
-    //     $this->load->library('pdf');
-    //     $pdf = $this->pdf->load();
-    //     $pdf->AddPage("L");
-    //     $stylesheet = file_get_contents('./assets/dashboard/css/style.css');
-
-    //     $pdf->WriteHTML($stylesheet, 1);
-    //     $pdf->WriteHTML($pdf_view, 2);
-
-    //     $pdf->output('Dispatch List ' . date('d-m-Y') . '.pdf', 'D');
-    // }
-
-
-
 
 }
 
