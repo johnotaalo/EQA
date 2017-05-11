03987df33af6b06c13d2b520b606558cbb96506c
WillyMareka <marekawilly@gmail.com>
2017-05-12 00:09:36 +0300

    Admin can check for panel conditions by the participant

--- application/modules/PTRounds/views/paneltracking/track_v.php
+++ application/modules/PTRounds/views/paneltracking/track_v.php
@@ -101,113 +101,19 @@
 					<h4><?= @$tracking->status; ?></h4>
 				</td>
 			</tr>
+			<?php if($tracking->status_code == 3){?>
+			<tr>
+				<td>
+					<p>Panel Received on: </p>
+					<h4><?= @date('dS F, Y', strtotime($tracking->participant_received_date)); ?></h4>
+				</td>
+			</tr>
+			<?php } ?>
 		</table>
 
 		<?php if($tracking->status_code == 3){?>
 		<table class = "table">
-			<tr>
-				<th style="width: 30%;">Panel Received on: </th>
-				<td><?= @date('dS F, Y', strtotime($tracking->participant_received_date)); ?></td>
-			</tr>
-			<tr>
-				<th style="width: 30%;">Sample Tubes</th>
-				<td>
-					<?php
-						if($tracking->sample_tubes == 0){
-							echo "Broken";
-						}else if($tracking->sample_tubes == 1){
-							echo "Leaking";
-						}else if($tracking->sample_tubes == 2){
-							echo "Cracked";
-						}
-					?>
-				</td>
-			</tr>
-
-			<tr>
-				<th style="width: 30%;">Insufficient Volume</th>
-				<td>
-					<?php
-						if($tracking->insufficient_volume == 1){
-							echo "Yes";
-						}else if($tracking->insufficient_volume == 0){
-							echo "No";
-						}
-					?>
-				</td>
-			</tr>
-
-			<tr>
-				<th style="width: 30%;">Haemolysed sample</th>
-				<td>
-					<?php
-						if($tracking->haemolysed_sample == 1){
-							echo "Yes";
-						}else if($tracking->haemolysed_sample == 0){
-							echo "No";
-						}
-					?>
-				</td>
-			</tr>
-
-			<tr>
-				<th style="width: 30%;">Clotted sample</th>
-				<td>
-					<?php
-						if($tracking->clotted_sample == 1){
-							echo "Yes";
-						}else if($tracking->clotted_sample == 0){
-							echo "No";
-						}
-					?>
-				</td>
-			</tr>
-
-			<tr>
-				<th style="width: 30%;">Duplicate sample received</th>
-				<td>
-					<?php
-						if($tracking->duplicate_sample == 1){
-							echo "Yes";
-						}else if($tracking->duplicate_sample == 0){
-							echo "No";
-						}
-					?>
-				</td>
-			</tr>
-
-			<tr>
-				<th style="width: 30%;">Missing sample</th>
-				<td>
-					<?php
-						if($tracking->missing_sample == 1){
-							echo "Yes";
-						}else if($tracking->missing_sample == 0){
-							echo "No";
-						}
-					?>
-				</td>
-			</tr>
-
-			<tr>
-				<th style="width: 30%;">Mismatch of information details on introductory letter and sample tube</th>
-				<td>
-					<?php
-						if($tracking->mismatch == 1){
-							echo "Yes";
-						}else if($tracking->mismatch == 0){
-							echo "No";
-						}
-					?>
-				</td>
-			</tr>
-
-			<tr>
-				<th style="width: 30%;">Participant Comment</th>
-				<td>
-					<?= @$tracking->panel_condition_comment; ?>
-				</td>
-			</tr>
+			<?= @$conditions; ?>
 		</table>
 		<?php } ?>
 	</div>
--- application/modules/Participant/views/test.php
+++ application/modules/Participant/views/test.php
@@ -1,255 +1,100 @@
-<?php $assets_url = $this->config->item('assets_url'); ?>
 
-    <div class="blog-area inner-padding" id="blog">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12">
-                    <div class="section-title-1">
-                        <h2 class="title-1">Frequently Asked Quuestions</h2>
-                        <p><!-- Lorem provide best expert consectetur adipiscing elit, sed do eiusmod tempor dolor sit amet adipiscing elit, sed do eiusmod tempor incididunt ut labore et lor gna aliqua.  --></p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-12 col-md-6">
-                    <div class="post-row mb30">
-                        <div class="post-header">
-                            <div class="post-feature">
-                                <img src="<?= @$assets_url; ?>frontend/images/files/FAQS/ENROLMENT.png" alt="responsive img">
-                            </div>
-                            <div class="post-sticker"><small>FAQ</small>
-                                <p class="month">1</p>
-                            </div>
-                        </div>
-                        <div class="post-body">
-                            <div class="post-caption">
-                                <h2 class="post-heading"><a href="#">Why should I enrol?</a></h2>
-                            </div>
-                            <p class="post-text">Proficiency testing offers an objective way to monitor and assess the reliability of laboratory results you generate for clinical care and management. It can also be used for evaluation of competency</p>
-                        </div>
-                        <div class="post-footer">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-6">
-                    <div class="post-row mb0">
-                        <div class="post-header">
-                            <div class="post-feature">
-                                <img src="<?= @$assets_url; ?>frontend/images/files/FAQS/PROGRAMMES.png" alt="responsive img">
-                            </div>
-                            <div class="post-sticker"><small>FAQ</small>
-                                <p class="month">2</p>
-                            </div>
-                        </div>
-                        <div class="post-body">
-                            <div class="post-caption">
-                                <h2 class="post-heading"><a href="#">Which programmes are currently available under the scheme?</a></h2>
-                                <!-- <div class="post-meta">Martino / Design, at 2 pm</div> -->
-                            </div>
-                            <p class="post-text">The NHRL proficiency testing programme currently offers the following modules:</p>
-                            <p>
-                                <ul>
-                                    <li>
-                                        HIV Early Infant Diagnosis
-                                    </li>
-                                    <li>
-                                        HIV Viral Load
-                                    </li>
-                                    <li>
-                                        Flow Cytometry
-                                    </li>
-                                </ul>
-                            </p>
-                        </div>
-                        <div class="post-footer">
-                            <!-- <a href="#" class="btn btn-default btn-post" role="button">READ MORE<i class="fa fa-angle-double-right"></i></a> -->
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-6">
-                    <div class="post-row mb0">
-                        <div class="post-header">
-                            <div class="post-feature">
-                                <img src="<?= @$assets_url; ?>frontend/images/files/FAQS/ELIGIBILITY.png" alt="responsive img">
-                            </div>
-                            <div class="post-sticker"><small>FAQ</small>
-                                <p class="month">3</p>
-                            </div>
-                        </div>
-                        <div class="post-body">
-                            <div class="post-caption">
-                                <h2 class="post-heading"><a href="#">Who is eligible for enrolment?</a></h2>
-                                <!-- <div class="post-meta">Martino / Design, at 2 pm</div> -->
-                            </div>
-                            <p class="post-text">Laboratory personnel in clinical or research laboratories involved in medical investigations for patient care and management</p>
-                        </div>
-                        <div class="post-footer">
-                            <!-- <a href="#" class="btn btn-default btn-post" role="button">READ MORE<i class="fa fa-angle-double-right"></i></a> -->
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-6">
-                    <div class="post-row mb0">
-                        <div class="post-header">
-                            <div class="post-feature">
-                                <img src="<?= @$assets_url; ?>frontend/images/files/FAQS/REGISTRATION.png" alt="responsive img">
-                            </div>
-                            <div class="post-sticker"><small>FAQ</small>
-                                <p class="month">4</p>
-                            </div>
-                        </div>
-                        <div class="post-body">
-                            <div class="post-caption">
-                                <h2 class="post-heading"><a href="#">How do I register?</a></h2>
-                                <!-- <div class="post-meta">Martino / Design, at 2 pm</div> -->
-                            </div>
-                            <p class="post-text">Visit the National Public Health Laboratories website: <a href="http://www.nphls.or.ke/">www.nphls.or.ke</a>. Click on Portal, select NHRL PT and select the module you wish to enroll, from the drop down menu. Fill in the form online. An automatic response confirmation email will be sent to you after the coordination team at NHRL has verified the submitted information.</p>
-                        </div>
-                        <div class="post-footer">
-                            <!-- <a href="#" class="btn btn-default btn-post" role="button">READ MORE<i class="fa fa-angle-double-right"></i></a> -->
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-6">
-                    <div class="post-row mb0">
-                        <div class="post-header">
-                            <div class="post-feature">
-                                <img src="<?= @$assets_url; ?>frontend/images/files/FAQS/ROUND.png" alt="responsive img">
-                            </div>
-                            <div class="post-sticker"><small>FAQ</small>
-                                <p class="month">5</p>
-                            </div>
-                        </div>
-                        <div class="post-body">
-                            <div class="post-caption">
-                                <h2 class="post-heading"><a href="#">Do I need to register for each round?</a></h2>
-                                <!-- <div class="post-meta">Martino / Design, at 2 pm</div> -->
-                            </div>
-                            <p class="post-text">No. You only register once. Your account is maintained. However you will be required to fill in a readiness assessment evaluation for each round.</p>
-                        </div>
-                        <div class="post-footer">
-                            <!-- <a href="#" class="btn btn-default btn-post" role="button">READ MORE<i class="fa fa-angle-double-right"></i></a> -->
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-6">
-                    <div class="post-row mb0">
-                        <div class="post-header">
-                            <div class="post-feature">
-                                <img src="<?= @$assets_url; ?>frontend/images/files/FAQS/SWITCH.png" alt="responsive img">
-                            </div>
-                            <div class="post-sticker"><small>FAQ</small>
-                                <p class="month">6</p>
-                            </div>
-                        </div>
-                        <div class="post-body">
-                            <div class="post-caption">
-                                <h2 class="post-heading"><a href="#">How do I switch to a different test?</a></h2>
-                                <!-- <div class="post-meta">Martino / Design, at 2 pm</div> -->
-                            </div>
-                            <p class="post-text">Register for the module you wish to switch to just like a new user
-                            </p>
-                        </div>
-                        <div class="post-footer">
-                            <!-- <a href="#" class="btn btn-default btn-post" role="button">READ MORE<i class="fa fa-angle-double-right"></i></a> -->
-                        </div>
-                    </div>
-                </div>
+            <tr>
+                <th style="width: 30%;">Sample Tubes</th>
+                <td>
+                    <?php
+                        if($tracking->sample_tubes == 0){
+                            echo "Broken";
+                        }else if($tracking->sample_tubes == 1){
+                            echo "Leaking";
+                        }else if($tracking->sample_tubes == 2){
+                            echo "Cracked";
+                        }
+                    ?>
+                </td>
+            </tr>
 
-                <div class="col-sm-12 col-md-6">
-                    <div class="post-row mb0">
-                        <div class="post-header">
-                            <div class="post-feature">
-                                <img src="<?= @$assets_url; ?>frontend/images/files/FAQS/PERFORMANCE_EVALUATION.png" alt="responsive img">
-                            </div>
-                            <div class="post-sticker"><small>FAQ</small>
-                                <p class="month">7</p>
-                            </div>
-                        </div>
-                        <div class="post-body">
-                            <div class="post-caption">
-                                <h2 class="post-heading"><a href="#">How is my performance evaluated?</a></h2>
-                                <!-- <div class="post-meta">Martino / Design, at 2 pm</div> -->
-                            </div>
-                            <p class="post-text">Performance is graded as either <strong>Acceptable</strong> or <strong>Unacceptable</strong> using the criteria of being within or out of ± 2 standard deviations of the peer mean which is calculated per platform</p>
-                        </div>
-                        <div class="post-footer">
-                            <!-- <a href="#" class="btn btn-default btn-post" role="button">READ MORE<i class="fa fa-angle-double-right"></i></a> -->
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="col-sm-12 col-md-6">
-                    <div class="post-row mb0">
-                        <div class="post-header">
-                            <div class="post-feature">
-                                <img src="<?= @$assets_url; ?>frontend/images/files/FAQS/UNSATISFACTORY_RESULT.png" alt="responsive img">
-                            </div>
-                            <div class="post-sticker"><small>FAQ</small>
-                                <p class="month">8</p>
-                            </div>
-                        </div>
-                        <div class="post-body">
-                            <div class="post-caption">
-                                <h2 class="post-heading"><a href="#">What do I do if I get an unsatisfactory result?</a></h2>
-                                <!-- <div class="post-meta">Martino / Design, at 2 pm</div> -->
-                            </div>
-                            <p class="post-text">Perform a root cause analysis followed by corrective action and where applicable, preventive action. Fill in the form provided online and submit within the stipulated duration</p>
-                        </div>
-                        <div class="post-footer">
-                            <!-- <a href="#" class="btn btn-default btn-post" role="button">READ MORE<i class="fa fa-angle-double-right"></i></a> -->
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-6">
-                    <div class="post-row mb0">
-                        <div class="post-header">
-                            <div class="post-feature">
-                                <img src="<?= @$assets_url; ?>frontend/images/files/FAQS/REPORT_CONFIDENTIALTY.png" alt="responsive img">
-                            </div>
-                            <div class="post-sticker"><small>FAQ</small>
-                                <p class="month">9</p>
-                            </div>
-                        </div>
-                        <div class="post-body">
-                            <div class="post-caption">
-                                <h2 class="post-heading"><a href="#">Are the performance reports confidential?</a></h2>
-                                <!-- <div class="post-meta">Martino / Design, at 2 pm</div> -->
-                            </div>
-                            <p class="post-text">Individual performance reports are confidential. It is the responsibility of the participant to secure their log in credentials to prevent unauthorized access to their account</p>
-                        </div>
-                        <div class="post-footer">
-                            <!-- <a href="#" class="btn btn-default btn-post" role="button">READ MORE<i class="fa fa-angle-double-right"></i></a> -->
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-12 col-md-6">
-                    <div class="post-row mb0">
-                        <div class="post-header">
-                            <div class="post-feature">
-                                <img src="<?= @$assets_url; ?>frontend/images/files/FAQS/CONTACT_US.png" alt="responsive img">
-                            </div>
-                            <div class="post-sticker"><small>FAQ</small>
-                                <p class="month">10</p>
-                            </div>
-                        </div>
-                        <div class="post-body">
-                            <div class="post-caption">
-                                <h2 class="post-heading"><a href="#">How can the administrators of this EQA scheme be reached?</a></h2>
-                                <!-- <div class="post-meta">Martino / Design, at 2 pm</div> -->
-                            </div>
-                            <p class="post-text">Use the Contact us tab at the top right of the screen</p>
-                        </div>
-                        <div class="post-footer">
-                            <!-- <a href="#" class="btn btn-default btn-post" role="button">READ MORE<i class="fa fa-angle-double-right"></i></a> -->
-                        </div>
-                    </div>
-                </div>
+            <tr>
+                <th style="width: 30%;">Insufficient Volume</th>
+                <td>
+                    <?php
+                        if($tracking->insufficient_volume == 1){
+                            echo "Yes";
+                        }else if($tracking->insufficient_volume == 0){
+                            echo "No";
+                        }
+                    ?>
+                </td>
+            </tr>
 
+            <tr>
+                <th style="width: 30%;">Haemolysed sample</th>
+                <td>
+                    <?php
+                        if($tracking->haemolysed_sample == 1){
+                            echo "Yes";
+                        }else if($tracking->haemolysed_sample == 0){
+                            echo "No";
+                        }
+                    ?>
+                </td>
+            </tr>
 
+            <tr>
+                <th style="width: 30%;">Clotted sample</th>
+                <td>
+                    <?php
+                        if($tracking->clotted_sample == 1){
+                            echo "Yes";
+                        }else if($tracking->clotted_sample == 0){
+                            echo "No";
+                        }
+                    ?>
+                </td>
+            </tr>
 
+            <tr>
+                <th style="width: 30%;">Duplicate sample received</th>
+                <td>
+                    <?php
+                        if($tracking->duplicate_sample == 1){
+                            echo "Yes";
+                        }else if($tracking->duplicate_sample == 0){
+                            echo "No";
+                        }
+                    ?>
+                </td>
+            </tr>
 
+            <tr>
+                <th style="width: 30%;">Missing sample</th>
+                <td>
+                    <?php
+                        if($tracking->missing_sample == 1){
+                            echo "Yes";
+                        }else if($tracking->missing_sample == 0){
+                            echo "No";
+                        }
+                    ?>
+                </td>
+            </tr>
 
-            </div>
-        </div>
-    </div>
+            <tr>
+                <th style="width: 30%;">Mismatch of information details on introductory letter and sample tube</th>
+                <td>
+                    <?php
+                        if($tracking->mismatch == 1){
+                            echo "Yes";
+                        }else if($tracking->mismatch == 0){
+                            echo "No";
+                        }
+                    ?>
+                </td>
+            </tr>
+
+            <tr>
+                <th style="width: 30%;">Participant Comment</th>
+                <td>
+                    <?= @$tracking->panel_condition_comment; ?>
+                </td>
+            </tr>
